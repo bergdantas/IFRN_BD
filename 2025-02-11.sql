f8f958351af625ef5a97c4e377ced1185c68f04e
bergdantas <54481994+bergdantas@users.noreply.github.com>
2025-02-11 21:25:20 -0300

    2025-02-11.sql

--- 2025-02-11.sql
+++ 2025-02-11.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS carro (
 );
 
 --crio a tabela endereço
-CREATE TABLE IF NOT EXISTS endereco(
+CREATE TABLE endereco(
 	cidade VARCHAR(15) NOT NULL,
 	rua VARCHAR(15) NOT NULL,
 	numero VARCHAR(5) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS endereco(
 );
 
 --crio a tabela vende
-CREATE TABLE IF NOT EXISTS vende(
+CREATE TABLE vende(
 	idVenda SERIAL PRIMARY KEY,
 	idPessoa INT NOT NULL,
 	idCarro INT NOT NULL,
@@ -50,3 +50,26 @@ DROP COLUMN cor;
 --apago as duas tabelas criadas anteriormente
 DROP TABLE telefone;
 DROP TABLE pessoa;
+
+--inserindo vendedores na base de dados
+INSERT INTO vendedor(nome) VALUES ('João Dias');
+INSERT INTO vendedor(nome) VALUES ('Marluce Josefina');
+INSERT INTO vendedor(nome) VALUES ('Jorge Felix');
+
+INSERT INTO telefone(idPessoa, numero) VALUES (1, '84956254875');
+INSERT INTO telefone(idPessoa, numero) VALUES (1, '84985458754');
+INSERT INTO telefone(idPessoa, numero) VALUES (2, '84985425478');
+INSERT INTO telefone(idPessoa, numero) VALUES (2, '84955875457');
+INSERT INTO telefone(idPessoa, numero) VALUES (2, '84958754215');
+INSERT INTO telefone(idPessoa, numero) VALUES (3, '84987542157');
+
+--alterando o nome de um vendedor
+UPDATE vendedor
+SET nome = 'Marluce Barros'
+WHERE idPessoa = 2;
+
+--deletando um vendedor
+DELETE FROM telefone
+WHERE idPessoa = 3
+DELETE FROM vendedor
+WHERE idPessoa = 3;
